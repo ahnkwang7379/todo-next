1483c551e6cc36ee49ca533bb1dd3fe680e02d59
ahnkwang7379 <61385865+ahnkwang7379@users.noreply.github.com>
2025-01-18 17:37:50 +0900

    first commit 초기 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next-Todo",
+  description: "simple todo app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body >
+        {children}
+      </body>
+    </html>
+  );
+}
