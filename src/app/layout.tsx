13db62e67ff10f714b0206cfb5ce96745a3cd587
ahnkwang7379 <61385865+ahnkwang7379@users.noreply.github.com>
2025-01-19 14:31:38 +0900

    [feat] prettier세팅 & 테스트용 auth페이지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Next-Todo",
-  description: "simple todo app",
-};
+  title: 'Next-Todo',
+  description: 'simple todo app',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import styles from "./page.module.scss";
+import styles from './page.module.scss'
 
 export default function Home() {
-  return <div className={styles.home} />;
+  return <div className={styles.home} />
 }
